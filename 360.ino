5d1557234e54756342e98fec62e969c07dc07a41
Chenyi Keyser <70242316+chenyikeyser@users.noreply.github.com>
2021-06-16 17:16:58 -0400

    Create 360.ino

---
+++ 360.ino
@@ -0,0 +1,43 @@
+//Include the Arduino Stepper Library
+#include <Stepper.h>
+ 
+// Define Constants
+ 
+// Number of steps per internal motor revolution 
+const float STEPS_PER_REV = 32; 
+ 
+//  Amount of Gear Reduction
+const float GEAR_RED = 64;
+ 
+// Number of steps per geared output rotation
+const float STEPS_PER_OUT_REV = STEPS_PER_REV * GEAR_RED;
+ 
+// Define Variables
+
+int i = 0;
+
+// Number of Steps Required
+int StepsRequired;
+ 
+Stepper steppermotor(STEPS_PER_REV, 8, 10, 9, 11);
+ 
+void setup()
+{
+// Nothing  (Stepper Library sets pins as outputs)
+}
+ 
+void loop()
+{
+
+  while (i < 61) 
+    {
+       // Rotate CCW X-amount Turns
+      StepsRequired  =  - STEPS_PER_OUT_REV / 60;   
+      steppermotor.setSpeed(200);  
+      steppermotor.step(StepsRequired);
+      delay(50);
+      i++;
+      
+      //take picture
+    }
+}
